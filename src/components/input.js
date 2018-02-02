2355b198bbafa4a2b4a4ce56f288578d2a241236
Thien Vo <jaysonvo97@gmail.com>
2018-02-02 10:36:01 -0600

    add input component

---
+++ src/components/input.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import {AppBar, Typography} from 'material-ui'
+
+
+
+export default class Input extends Component {
+  render() {
+
+    return (
+      <div>
+        Input to be completed
+      </div>
+    )
+  }
+}
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
-import NavBar from '../components/navbar'
+import NavBar from '../components/navbar';
+import Input from '../components/input'
 
 
 export default class Home extends Component {
@@ -9,7 +10,7 @@ export default class Home extends Component {
     return (
       <div>
         <NavBar/>
-        {/*Hello from Home!*/}
+        <Input/>
       </div>
     )
   }
