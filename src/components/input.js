ced26002136bb9889682a997a189c8b4732d12c7
Thien Vo <jaysonvo97@gmail.com>
2018-02-02 10:47:35 -0600

    basic input layout to be implemented

--- src/components/input.js
+++ src/components/input.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {AppBar, Typography} from 'material-ui'
+import {TextField, Typography} from 'material-ui'
 
 
 
@@ -8,7 +8,31 @@ export default class Input extends Component {
 
     return (
       <div>
-        Input to be completed
+        <TextField
+          id="address1"
+          label="Address #1"
+          InputLabelProps={{
+            shrink: true,
+            required: true
+          }}
+          placeholder="Please enter your first address..."
+          fullWidth
+          required
+          margin="normal"
+        />
+
+        <TextField
+          id="address2"
+          label="Address #2"
+          InputLabelProps={{
+            shrink: true,
+            required: true
+          }}
+          placeholder="Please enter your second address..."
+          fullWidth
+          required
+          margin="normal"
+        />
       </div>
     )
   }
