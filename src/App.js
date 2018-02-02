1d27e9e0a988b2c23921c01c4ce477bca77b6cff
Thien Vo <jaysonvo97@gmail.com>
2018-02-02 10:05:39 -0600

    add router function to app.js and index.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Route, Switch } from 'react-router-dom';
+
+const Main = () => (
+  <main>
+    <Switch>
+      {/*<Route exact path='/' component={Home}/>*/}
+      {/*<Route path='*' exact={true} component={Page404}/>*/}
+    </Switch>
+  </main>
+);
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Main/>
       </div>
     );
   }
