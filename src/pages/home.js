4d1a93958b5281394419804e5bfdb88e6352dd2d
Thien Vo <jaysonvo97@gmail.com>
2018-02-02 11:29:38 -0600

    result list using gridlist

--- src/pages/home.js
+++ src/pages/home.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 import NavBar from '../components/navbar';
 import Input from '../components/input'
+import ResultList from '../components/resultlist'
 
 
 export default class Home extends Component {
@@ -11,6 +12,7 @@ export default class Home extends Component {
       <div>
         <NavBar/>
         <Input/>
+        <ResultList/>
       </div>
     )
   }
