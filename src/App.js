264d1d91141e844979f1a87641ff98251833b9cf
Thien Vo <jaysonvo97@gmail.com>
2018-02-02 10:13:41 -0600

    template for home/navbar connection

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Route, Switch } from 'react-router-dom';
+import Home from './pages/home';
 
 const Main = () => (
   <main>
     <Switch>
-      {/*<Route exact path='/' component={Home}/>*/}
+      <Route exact path='/' component={Home}/>
       {/*<Route path='*' exact={true} component={Page404}/>*/}
     </Switch>
   </main>
---
+++ src/pages/home.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import NavBar from '../components/navbar'
+
+
+export default class Home extends Component {
+  render() {
+
+    return (
+      <div>
+        <NavBar/>
+        Hello from Home!
+      </div>
+    )
+  }
+}
