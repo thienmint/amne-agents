593c82e7fd3252112ce0a61aef47d6480d4beb63
Thien Vo <jaysonvo97@gmail.com>
2018-02-03 13:02:12 -0600

    pretty much done with all error checking for input component

--- src/components/input.js
+++ src/components/input.js
@@ -1,71 +1,144 @@
 import React, { Component } from 'react';
-import {InputLabel, Button, CircularProgress} from 'material-ui'
+import {InputLabel, Button} from 'material-ui'
+import {FormHelperText, Divider} from 'material-ui'
 import PlacesAutocomplete, { geocodeByAddress, getLatLng } from 'react-places-autocomplete'
 import async from 'async'
 
 
 
 export default class InputForm extends Component {
+  // TODO add addres validation by only allowing them to pick from the list
   constructor(props) {
     super(props);
     this.state = {
-      address: {
-        1: 'Austin, TX',
-        2: 'Austin, TX'
+      addressInput: {
+        1: {
+          'address': 'Austin, TX',
+          'validated': true,
+          'inputHelpText': ''
+        },
+        2: {
+          'address': 'Dallas, TX',
+          'validated': true,
+          'inputHelpText': ''
+        },
       },
-      submitLocations: [],
+      submitLocations: {},
       waitingSubmit: false
     };
+    // Define function repeatedly used in components
     this.onChange1 = (address) => this.handleChange(1, address);
     this.onChange2 = (address) => this.handleChange(2, address);
+    this.onSelect1 = (address, placeId) => this.handleSelect(1, address);
+    this.onSelect2 = (address, placeId) => this.handleSelect(2, address);
+    this.onNonSelect1 = (address, placeId) => this.handleNonSelect(1);
+    this.onNonSelect2 = (address, placeId) => this.handleNonSelect(2);
+
+    // Handler functions
     this.handleChange = this.handleChange.bind(this);
+    this.handleSelect = this.handleSelect.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleReset = this.handleReset.bind(this);
   }
+
+  /**
+   * Update the input address component as the user types
+   * @param addressNum the ordering of the input
+   * @param newAddress the new address to change in state
+   */
   handleChange (addressNum, newAddress) {
     let stateCopy = Object.assign({}, this.state);
-    stateCopy.address[addressNum] = newAddress;
+    stateCopy.addressInput[addressNum]['address'] = newAddress;
+    stateCopy.addressInput[addressNum]['validated'] = false;
     this.setState(stateCopy)
   }
 
+
+  /**
+   * When select an address from the autocomplete list, the address will be marked validated.
+   * @param addressNum the ordering of the input
+   * @param newAddress the new address to change in state
+   */
+  handleSelect (addressNum, newAddress) {
+    let stateCopy = Object.assign({}, this.state);
+    stateCopy.addressInput[addressNum]['address'] = newAddress;
+    stateCopy.addressInput[addressNum]['validated'] = true;
+    stateCopy.addressInput[addressNum]['inputHelpText'] = '';
+    this.setState(stateCopy)
+  }
+
+
+  /**
+   * Validate the input to make sure the user select a suggested address
+   * @param addressNum the ordering of the input
+   */
+  handleNonSelect (addressNum) {
+    let stateCopy = Object.assign({}, this.state);
+    if (!stateCopy.addressInput[addressNum]['validated'])
+      stateCopy.addressInput[addressNum]['inputHelpText'] = "Please select an address from autocomplete list!";
+    else
+      stateCopy.addressInput[addressNum]['inputHelpText'] = "";
+    this.setState(stateCopy)
+  }
+
+
+  /**
+   * Handle reset form back to default
+   */
+  handleReset() {
+    let stateCopy = Object.assign({}, this.state);
+    stateCopy.addressInput = {
+      1: {
+        'address': 'Austin, TX',
+        'validated': true,
+        'inputHelpText': ''
+      },
+      2: {
+        'address': 'Dallas, TX',
+        'validated': true,
+        'inputHelpText': ''
+      },
+    }
+    this.setState(stateCopy)
+  }
+
+
+  /**
+   * Handle submission
+   */
   handleSubmit() {
-    this.setState({waitingSubmit: true});
+    this.setState((state) => ({waitingSubmit: true}));
+
     const object = this;
 
     let stateCopy = Object.assign({}, this.state);
     stateCopy.submitLocations = [];
 
-    let geoCode1 = geocodeByAddress(stateCopy.address[1]);
-    let geoCode2 =geocodeByAddress(stateCopy.address[2]);
-
-    async.every([geoCode1, geoCode2], function (geoCodePromise, callback) {
-      geoCodePromise
+    async.every([1,2], function(addressNum, callback) {
+      geocodeByAddress(stateCopy.addressInput[addressNum]['address'])
         .then(results => getLatLng(results[0]))
         .then(latLng => {
-          stateCopy.submitLocations.push(latLng);
-          console.log("Got a result", latLng);
+          stateCopy.submitLocations[addressNum] = latLng;
           callback(null, true)
         })
         .catch(error => {
-          console.log("Error getting long/lat", error);
+          stateCopy.addressInput[addressNum]['inputHelpText'] = "Error getting long/lat. Try another address or try again!";
           callback(error, false)
         });
     }, function (err, result) {
-      if (err) {console.log(err); return;}
-
-      // TODO: Add submission state (empty, failed, waiting, success)
+      if (err) {
+        stateCopy.addressInput[1]['inputHelpText'] = "Error getting long/lat. Try another address or try again!";
+        stateCopy.addressInput[2]['inputHelpText'] = "Error getting long/lat. Try another address or try again!";
+        console.log(err);
+      }
       stateCopy.waitingSubmit = false;
+
+      // Set state to report any errors
       object.setState(stateCopy);
-      object.props.onSubmit(stateCopy.submitLocations);
-    })
-
-  }
-
-  handleReset() {
-    let stateCopy = Object.assign({}, this.state);
-    stateCopy.address[1] = 'Austin, TX';
-    stateCopy.address[2] = 'Austin, TX';
-    this.setState(stateCopy)
+      // If succeed, then return data to parent
+      if (result)
+        object.props.afterSubmit(stateCopy.submitLocations);
+    });
   }
 
   render() {
@@ -105,41 +178,62 @@ export default class InputForm extends Component {
       <div>
         <InputLabel>Address #1</InputLabel>
         <PlacesAutocomplete
-          name="firstAddress"
-          inputProps={{value: this.state.address[1], onChange: this.onChange1}}
+          inputProps={
+            {
+              name: 'firstAddress',
+              value: this.state.addressInput[1]['address'],
+              onChange: this.onChange1,
+              onBlur: this.onNonSelect1,
+              type: 'search',
+              placeholder: 'Please enter and select an address',
+              required: true
+            }
+          }
           renderSuggestion={renderSuggestion}
           styles={myStyles}
+          onSelect={this.onSelect1}
+          onError={(status, clearSuggestions) => {
+            console.log('Google Maps API returned error with status: ', status);
+            clearSuggestions()
+          }}
+          onEnterKeyDown={this.onNonSelect1}
         />
+        <FormHelperText margin='dense' error>{this.state.addressInput[1]['inputHelpText']}</FormHelperText>
 
         <InputLabel>Address #2</InputLabel>
         <PlacesAutocomplete
-          name="secondAddress"
-          inputProps={{value: this.state.address[2], onChange: this.onChange2}}
+          inputProps={
+            {
+              name: 'secondAddress',
+              value: this.state.addressInput[2]['address'],
+              onChange: this.onChange2,
+              onBlur: this.onNonSelect2,
+              type: 'search',
+              placeholder: 'Please enter and select an address',
+              required: true
+            }
+          }
           renderSuggestion={renderSuggestion}
           styles={myStyles}
+          onSelect={this.onSelect2}
+          onError={(status, clearSuggestions) => {
+            console.log('Google Maps API returned error with status: ', status);
+            clearSuggestions()
+          }}
+          onEnterKeyDown={this.onNonSelect2}
         />
+        <FormHelperText margin='dense' error>{this.state.addressInput[2]['inputHelpText']}</FormHelperText>
 
-        <Button
-          color = 'primary'
-          raised
-          size = 'small'
-          onClick={this.handleSubmit}
-          disabled={this.state.waitingSubmit}
-        >
+        <Button color = 'primary' raised size = 'small'
+                onClick={this.handleSubmit}
+                disabled={this.state.waitingSubmit |
+                (!this.state.addressInput[1]['validated'] | !this.state.addressInput[2]['validated'])}>
           Submit
         </Button>
-        <Button
-          color = 'secondary'
-          raised
-          size = 'small'
-          onClick={this.handleReset}
-          disable={this.state.waitingSubmit}
-        >
+        <Button color = 'secondary' raised size = 'small'
+                onClick={this.handleReset} disable={this.state.waitingSubmit}>
           Reset
         </Button>
-
-        {this.state.waitingSubmit && <CircularProgress size={25}/>}
-
       </div>
     )
   }
--- src/pages/home.js
+++ src/pages/home.js
@@ -19,7 +19,20 @@ export default class Home extends Component {
   }
 
   handleSubmission (submitLocations) {
-    console.log(submitLocations)
+    /**
+     * Get Yelp data here
+     * Build up a unique set of locations
+     * Build up unique dictionary
+     */
+    console.log("Got submitted locations", submitLocations)
+    console.log("Mid point = ", Home.findMidPoint(submitLocations[1], submitLocations[2]))
+  }
+
+  static findMidPoint (coor1, coor2) {
+    return {
+      'lat': (coor1['lat'] + coor2['lat'])/2,
+      'lng': (coor1['lng'] + coor2['lng'])/2
+    }
   }
 
   render() {
@@ -27,20 +40,20 @@ export default class Home extends Component {
     return (
       <div>
         <Grid container>
-          <Grid item xs={12} sm={4} md={3}>
+          <Grid item xs={12} sm={6} md={4} >
             <Grid container>
               <Grid item xs={12}>
                 <NavBar/>
               </Grid>
               <Grid item xs={12}>
-                <Input onSubmit={this.handleSubmission}/>
+                <Input afterSubmit={this.handleSubmission}/>
               </Grid>
               <Grid item xs={12}>
                 <ResultList/>
               </Grid>
             </Grid>
           </Grid>
-          <Grid item xs={12} sm={8} md={9}>
+          <Grid item xs={12} sm={6} md={8} >
             <WebMap/>
           </Grid>
         </Grid>
