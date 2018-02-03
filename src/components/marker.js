7390e6583003bb41badc0c46045c74540cedaa07
Thien Vo <jaysonvo97@gmail.com>
2018-02-03 16:32:13 -0600

    basic click popover for result

--- src/components/marker.js
+++ src/components/marker.js
@@ -1,17 +1,70 @@
 import React, {Component} from 'react';
 
 import {Room} from 'material-ui-icons'
-
+import {Popover, Typography} from 'material-ui'
+import Card, { CardActions, CardContent } from 'material-ui/Card';
 export default class Marker extends Component {
+
   constructor(props) {
     super(props);
+    this.state = {
+      anchorEl: null,
+      popperOpen: false,
+    };
   }
 
-    render() {
-    const str = this.props.$hover ? "#000" : this.props.defaultColor;
+  handlePopoverOpen = event => {
+    this.setState({ anchorEl: event.target });
+  };
+
+  handlePopoverClose = () => {
+    this.setState({ anchorEl: null });
+  };
+
+  render() {
+    const { anchorEl } = this.state;
+    const open = !!anchorEl;
+
+
+    const str = this.props.hover ? "#000" : this.props.defaultColor;
 
     return (
-      <Room lat={this.props.lat} lng={this.props.lng} color={str}/>
+      <div>
+        <Room onClick={this.handlePopoverOpen}
+              lat={this.props.lat} lng={this.props.lng} color={str}/>
+        <div>
+          <Popover
+            open={open}
+            anchorEl={anchorEl}
+            anchorOrigin={{
+              vertical: 'bottom',
+              horizontal: 'left',
+            }}
+
+            transformOrigin={{
+              vertical: 'top',
+              horizontal: 'left',
+            }}
+            onClose={this.handlePopoverClose}
+          >
+            <Card>
+              <CardContent>
+                <Typography type="title" component="h2">
+                  Company name
+                </Typography>
+                <Typography type="body1">
+                  Distance from #1: 15 miles
+                </Typography>
+                <Typography type="body1">
+                  Distance from #2: 25 miles
+                </Typography>
+              </CardContent>
+            </Card>
+          </Popover>
+
+        </div>
+      </div>
+
     );
   }
 }
--- src/components/webmap.js
+++ src/components/webmap.js
@@ -3,20 +3,38 @@
  */
 import React, {Component} from 'react';
 import Marker from './marker';
-import {Room} from 'material-ui-icons'
 import GoogleMap from 'google-map-react';
+import controllable from 'react-controllables';
 
-export default class WebMap extends Component {
+const WebMap = controllable(['center', 'zoom', 'hoverKey', 'clickKey'])(
+class WebMap extends Component {
+  static defaultProps = {
+    center: [30.2396338, -97.728029],
+    zoom: 12
+  };
+
   constructor(props) {
     super(props);
 
-    this.state = {
-      center: [30.2396338, -97.728029],
-      zoom: 12,
-    };
-
     this.generateMarkers = this.generateMarkers.bind(this);
   }
+
+  _onBoundsChange = (center, zoom /* , bounds, marginBounds */) => {
+    this.props.onCenterChange(center);
+    this.props.onZoomChange(zoom);
+  };
+
+  _onChildClick = (key, childProps) => {
+    // this.props.onCenterChange([childProps.lat, childProps.lng]);
+  };
+
+  _onChildMouseEnter = (key /*, childProps */) => {
+    this.props.onHoverKeyChange(key);
+  };
+
+  _onChildMouseLeave = (/* key, childProps */) => {
+    this.props.onHoverKeyChange(null);
+  };
 
   generateMarkers() {
     let search = this.props.locations['search'];
@@ -26,16 +44,20 @@ export default class WebMap extends Component {
 
     let elems = [];
     if (Object.keys(midpoint).length > 0)
-          elems.push(<Marker key={'midpoint'} lat={midpoint.lat} lng={midpoint.lng} defaultColor={"action"}/>);
+          elems.push(<Marker key={'midpoint'} lat={midpoint.lat} lng={midpoint.lng}
+                             defaultColor={"action"} hover={this.props.hoverKey === 'midpoint'}/>);
 
     if (Object.keys(firstAddress).length > 0)
-      elems.push(<Marker key={'firstaddress'} lat={firstAddress.lat} lng={firstAddress.lng} defaultColor={"primary"}/>);
+      elems.push(<Marker key={'firstaddress'} lat={firstAddress.lat} lng={firstAddress.lng}
+                         defaultColor={"primary"} hover={this.props.hoverKey === 'firstaddress'}/>);
 
     if (Object.keys(secondAddress).length > 0)
-      elems.push(<Marker key={'secondaddress'} lat={secondAddress.lat} lng={secondAddress.lng} defaultColor={"primary"}/>);
+      elems.push(<Marker key={'secondaddress'} lat={secondAddress.lat} lng={secondAddress.lng}
+                         defaultColor={"primary"} hover={this.props.hoverKey === 'secondaddress'}/>);
 
     search.map((coor, index) => {
-      elems.push(<Marker key={index} lat={coor.latitude} lng={coor.longitude} defaultColor={"secondary"}/>)
+      elems.push(<Marker key={index} lat={coor.latitude} lng={coor.longitude}
+                         defaultColor={"secondary"} hover={this.props.hoverKey === index}/>)
     });
 
     return elems
@@ -54,9 +76,14 @@ export default class WebMap extends Component {
       <div className="webmap">
         <GoogleMap
           apiKey='AIzaSyALINww1dMSk0T_EXLNaJ3MPLDdEV02H-g'
-          center={WebMap.getCenter(this.props.locations['midpoint'])}
-          zoom={12}
+          // center={WebMap.getCenter(this.props.locations['midpoint'])}
+          center={this.props.center}
+          zoom={this.props.zoom}
           hoverDistance={20}
+          onBoundsChange={this._onBoundsChange}
+          onChildClick={this._onChildClick}
+          onChildMouseEnter={this._onChildMouseEnter}
+          onChildMouseLeave={this._onChildMouseLeave}
         >
           {this.generateMarkers()}
         </GoogleMap>
@@ -64,3 +91,6 @@ export default class WebMap extends Component {
     );
   }
 }
+);
+
+export default WebMap;
